92c39b474f1508971e4587ddc3615d5c76a5b665
Takagi <Shinya.Takagi@sony.com>
2023-09-16 11:32:11 +0900

    Sass variable override method adjustment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -107,7 +107,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  css: ['assets/main.scss'],
+  css: ['assets/settings.scss'],
   build: {
     transpile: ['vuetify'],
   },
@@ -115,8 +115,9 @@ export default defineNuxtConfig({
     'vite:extendConfig': (config) => {
       config.plugins!.push(
         vuetify({
+          autoImport: true,
           styles: {
-            configFile: 'assets/main.scss',
+            configFile: 'assets/settings.scss',
           },
         })
       );
